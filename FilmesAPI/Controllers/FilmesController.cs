f14c2e4b0e5c7bce6348e40ca91ebdc510fd2b8b
Samuel Bozio <samuelbozio@hotmail.com>
2023-10-10 20:09:50 -0300

    aprendendo api basico

---
+++ FilmesAPI/Controllers/FilmesController.cs
@@ -0,0 +1,49 @@
+﻿using FilmesAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FilmesAPI.Controllers
+{
+
+    [ApiController]
+    [Route("{controller}")]
+    public class FilmesController : ControllerBase
+    {
+
+        private static List<FilmeModel> Filmes = new List<FilmeModel>();
+        private static int Id = 0;
+
+
+        [HttpPost]
+        public List<FilmeModel> AddLivros(FilmeModel filme)
+        {
+            filme.Id = Id++;
+            Filmes.Add(filme);
+            return Filmes;
+        }
+
+        [HttpGet]
+        public IEnumerable<FilmeModel> ShowFilmes(int skip = 0, int take = 0)
+        {
+            return Filmes.Skip(skip).Take(take);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult FilmePorId(int Id)
+        {
+            var filme = Filmes.FirstOrDefault(x => x.Id == Id);
+            if (filme == null)
+                return NotFound();
+            else
+                return Ok();
+        }
+
+        [HttpDelete]
+        public void DeletaFilmes()
+        {
+            Filmes.Clear();
+        }
+
+
+
+    }
+}
---
+++ FilmesAPI/Models/FilmeModel.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmesAPI.Models
+{
+    public class FilmeModel
+    {
+
+
+        [Required(ErrorMessage = "Necessário informar o título.")]
+        [MaxLength(255)]
+        public string Titulo { get; set; }
+        [Required(ErrorMessage = "Necessário informar o gênero")]
+        [MaxLength(255)]
+        public string Genero { get; set; }1
+        [Required(ErrorMessage = "Necessário informar a duração.")]
+        public int Duracao { get; set; }
+        
+        public int Id { get; set; }
+
+
+
+
+    }
+}
